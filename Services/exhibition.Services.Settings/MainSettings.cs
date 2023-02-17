bca077d3f176caa6545d8624af2cc1c7f61cb51a
Попов Никита <nikita_popov@relex.ru>
2023-02-17 17:56:07 +0300

    add basic settings for project and docker support

---
+++ Services/exhibition.Services.Settings/MainSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exhibition.Services.Settings
+{
+    public class MainSettings
+    {
+        public string MainUrl { get; private set; }
+    }
+}
---
+++ System/Api/exhibition.Api/Configuration/ControllerConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace exhibition.Api.Configuration
+{
+    public static class ControllerConfiguration
+    {
+        public static IServiceCollection AddAppController(this IServiceCollection services)
+        {
+
+            services
+            .AddControllers()
+                ;
+
+            return services;
+        }
+        public static IEndpointRouteBuilder UseAppController(this IEndpointRouteBuilder app)
+        {
+            app.MapControllers();
+
+            return app;
+        }
+    }
+}
